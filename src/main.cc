8f5a496b92999cf250d111b987af3df4338534d3
anthonyaje <anthonyang86@gmail.com>
2017-02-24 22:38:04 -0500

    client and server stub is added to main.cc and orchestrator code

--- src/main.cc
+++ src/main.cc
@@ -1,10 +1,49 @@
 #include<iostream>
 #include "micronf_agent.h"
 
+
+#include <grpc++/grpc++.h>
+#include "micronf_agent.grpc.pb.h"
+
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::ServerWriter;
+using grpc::Status;
+
 using namespace std;
+using namespace rpc_agent;
+
+class AgentServiceImpl final : public RPC::Service {
+	Status rpc_create_ring(ServerContext* context, const CreateRingRequest* s, 
+													Errno* reply) override {
+		//TODO create dpdk ring
+		cout<<"rpc_create_ring is called"<<endl;
+
+		return Status::OK;
+	}
+};
+
+void RunAgent(){
+	//FIXME specify non-dpdk interface
+	std::string server_address("0.0.0.0:50051");
+  AgentServiceImpl service;
+
+  ServerBuilder builder;
+
+  builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+
+  builder.RegisterService(&service);
+
+  std::unique_ptr<Server> server(builder.BuildAndStart());
+  std::cout << "Agent(Server) listening on " << server_address << std::endl;
+
+  server->Wait();
+
+}
 
 int main(int argc, char* argv[]){
 	cout<<"Agent is running"<<endl;
-
+	RunAgent();	
 
 }
--- src/micronf_orchest.cc
+++ src/micronf_orchest.cc
@@ -1,8 +1,53 @@
 #include<iostream>
+#include <grpc++/grpc++.h>
+#include "micronf_agent.grpc.pb.h"
+
+using grpc::Channel;
+using grpc::Status;
+using grpc::ClientContext;
+using grpc::ClientReader;
 
 using namespace std;
+using namespace rpc_agent;
+
+
+class RpcAgent{
+	public:
+		RpcAgent(std::shared_ptr<Channel> channel)
+			: stub_(RPC::NewStub(channel)) {}
+
+		int rpc_create_ring(string ring_name){
+			ClientContext context;
+			CreateRingRequest req;
+			Errno result;
+			req.set_name(ring_name);
+			
+			Status status = stub_->rpc_create_ring(&context, req, &result);
+			if(result.err() != 0){
+					cout << "errno: " << result.err() <<endl;
+					return -result.err();
+			}
+			if(!status.ok()){
+					cout << "errno: " << result.err() <<endl;
+					return -1;
+			}
+
+			return 0;
+		}
+
+	
+	private:
+		std::unique_ptr<RPC::Stub> stub_;
+
+};
 
 int main(int argc, char* argv[]){
 	cout<<"this is orchestrator"<<endl;
+
+	RpcAgent* rpcAgent = new RpcAgent(grpc::CreateChannel(
+												"0.0.0.0:50051", grpc::InsecureChannelCredentials()));
+	
+	//TODO test rpc_create_ring
+	rpcAgent->rpc_create_ring("ring_u1_u2");
 
 }
