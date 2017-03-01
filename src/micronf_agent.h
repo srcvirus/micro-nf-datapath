094d354f6c31432cef0fc68c90e0ab8ef030a1b3
Faizul <faizulbari@gmail.com>
2017-02-28 22:01:47 -0500

    InitPort and CreateRing implemented! But not tested

--- src/micronf_agent.h
+++ src/micronf_agent.h
@@ -1,12 +1,15 @@
 #ifndef _MICRONF_AGENT_H_
 #define _MICRONF_AGENT_H_
 
+#include <string>
+#include <sys/types.h>
+
 class MicronfAgent {
   public:
-    MicronfAgent(){}
-    ~MicronfAgent(){}
+    MicronfAgent();
+    ~MicronfAgent();
     int Init(int argc, char* argv[]);
-    int CreateRing(); 
+    int CreateRing(std::string ring_name); 
     int DeployMicroServices();
     int StartMicroService();
     int StopMicroService();
@@ -16,7 +19,7 @@ class MicronfAgent {
   private:
     //int DeployOneMicroService();
 		int InitMbufPool();
-	
+		int InitPort(uint8_t);	
 		/* The mbuf pool for packet rx */
 		struct rte_mempool *pktmbuf_pool;
 
