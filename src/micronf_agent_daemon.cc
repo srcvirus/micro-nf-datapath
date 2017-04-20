76b388ae8b172f2346dc6b4093d2d1d11f9e9154
Shihab <shihab.buet@gmail.com>
2017-04-20 16:26:39 -0400

    running at red cluster

--- src/micronf_agent_daemon.cc
+++ src/micronf_agent_daemon.cc
@@ -69,21 +69,25 @@ int main(int argc, char* argv[]){
 	MicronfAgent micronfAgent;
 	micronfAgent.Init(argc, argv);
 	
-	std::string conf_folder_path = "../confs/";	
+	// std::string conf_folder_path = "/home/nfuser/dpdk_study/micro-nf-datapath/confs/";	
+	std::string conf_folder_path = "../confs/";
 	std::vector<std::string> chain_conf = {
-		conf_folder_path + "mac_swapper_test.conf"//,
+		conf_folder_path + "MacSwapper.conf"//,
 		//conf_folder_path + "mac_swapper_2.conf",
 		//conf_folder_path + "mac_swapper_3.conf",
 		//conf_folder_path + "mac_swapper_4.conf"
 	};
 	
-	micronfAgent.addAvailCore("0x40");	
-	micronfAgent.addAvailCore("0x02");	
-	micronfAgent.addAvailCore("0x80");	
+	micronfAgent.addAvailCore("0x400000");	
+	micronfAgent.addAvailCore("0x200000");	
+	micronfAgent.addAvailCore("0x100000");	
+	micronfAgent.addAvailCore("0x080000");	
+	micronfAgent.addAvailCore("0x800000");	
 	micronfAgent.DeployMicroservices(chain_conf);
 
-	int monitor_lcore_id = rte_get_next_lcore(rte_lcore_id(), 1, 1);
-  int nic_classifier_lcore_id = rte_get_next_lcore(monitor_lcore_id, 1, 1);
+	//int monitor_lcore_id = rte_get_next_lcore(rte_lcore_id(), 1, 1);
+	int monitor_lcore_id = 15;
+  	int nic_classifier_lcore_id = 18;
 	printf("master lcore: %d, monitor lcore: %d, nic_classifier lcore: %d\n", rte_lcore_id(), monitor_lcore_id, nic_classifier_lcore_id);
 
 	rte_eal_remote_launch(RunMonitor, reinterpret_cast<void*> (&micronfAgent),
